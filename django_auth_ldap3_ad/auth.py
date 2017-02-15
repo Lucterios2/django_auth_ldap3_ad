45e5e9519908e5ea2f1b87942a8751fa4f9ea79f
Ludwig Weiss <seyhbold@users.noreply.github.com>
2017-02-15 17:21:54 +0100

    Use mapped LDAP user attribute for lookup

--- django_auth_ldap3_ad/auth.py
+++ django_auth_ldap3_ad/auth.py
@@ -149,7 +149,8 @@ class LDAP3ADBackend(object):
                 try:
                     # try to retrieve user from database and update it
                     username_field = getattr(settings, 'LDAP_USER_MODEL_USERNAME_FIELD', 'username') 
-                    usr = user_model.objects.get(**{"{0}__iexact".format(username_field): username})
+                    lookup_username = user_attribs[settings.LDAP_ATTRIBUTES_MAP[username_field]]
+                    usr = user_model.objects.get(**{"{0}__iexact".format(username_field): lookup_username})
                 except user_model.DoesNotExist:
                     # user does not exist in database already, create it
                     usr = user_model()
