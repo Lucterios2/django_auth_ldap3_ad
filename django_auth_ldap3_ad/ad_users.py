2732adda3385f3c30c278c039899742593e026f5
Pierre-Olivier VERSCHOORE <po.verschoore@sgigroupe.com>
2017-03-24 15:29:11 +0100

    - Add constants to ease the use of the userAccountControl field in AD - use the new constants in the create and activate user fonctions - add parameter to the activate function to enable to activate with "password never expires"

--- django_auth_ldap3_ad/ad_users.py
+++ django_auth_ldap3_ad/ad_users.py
@@ -38,6 +38,30 @@ logger = logging.getLogger(__name__)
 
 
 class Aduser:
+
+    # Constants for AD userAccountControl:
+    ADS_UF_ACCOUNT_DISABLE = 2
+    ADS_UF_HOMEDIR_REQUIRED = 8
+    ADS_UF_LOCKOUT = 16
+    ADS_UF_PASSWD_NOTREQD = 32
+    ADS_UF_PASSWD_CANT_CHANGE = 64
+    ADS_UF_ENCRYPTED_TEXT_PASSWORD_ALLOWED = 128
+    ADS_UF_NORMAL_ACCOUNT = 512
+    ADS_UF_INTERDOMAIN_TRUST_ACCOUNT = 2048
+    ADS_UF_WORKSTATION_TRUST_ACCOUNT = 4096
+    ADS_UF_SERVER_TRUST_ACCOUNT = 8192
+    ADS_UF_DONT_EXPIRE_PASSWD = 65536
+    ADS_UF_MNS_LOGON_ACCOUNT = 131072
+    ADS_UF_SMARTCARD_REQUIRED = 262144
+    ADS_UF_TRUSTED_FOR_DELEGATION = 524288
+    ADS_UF_NOT_DELEGATED = 1048576
+    ADS_UF_USE_DES_KEY_ONLY = 2097152
+    ADS_UF_DONT_REQUIRE_PREAUTH = 4194304
+    ADS_UF_PASSWORD_EXPIRED = 8388608
+    ADS_UF_TRUSTED_TO_AUTHENTICATE_FOR_DELEGATION = 16777216
+    ADS_UF_NO_AUTH_DATA_REQUIRED = 33554432
+    ADS_UF_PARTIAL_SECRETS_ACCOUNT = 67108864
+
     def __init__(self):
         self.pool = None
         self.con = None
@@ -77,7 +101,7 @@ class Aduser:
             'sn': lastname,
             'displayName': '%s %s' % (firstname, lastname),
             'sAMAccountName': samaccountname,
-            'userAccountControl': '514',
+            'userAccountControl': Aduser.ADS_UF_ACCOUNT_DISABLE + Aduser.ADS_UF_NORMAL_ACCOUNT,
             'distinguishedName': user_dn,
             'userPrincipalName': "%s@%s" % (samaccountname, settings.LDAP_AD_DOMAIN)
             # 514 will set user account to disabled, 512 is enable but can't create directly
@@ -110,8 +134,11 @@ class Aduser:
         )
         return self.con.result
 
-    def activate_ad_user(self, user_dn):
-        return self.update_ad_user(user_dn, {"userAccountControl": '512'})
+    def activate_ad_user(self, user_dn, never_expires=False):
+        if never_expires:
+            return self.update_ad_user(user_dn, {"userAccountControl":
+                                                     Aduser.ADS_UF_NORMAL_ACCOUNT + Aduser.ADS_UF_DONT_EXPIRE_PASSWD})
+        return self.update_ad_user(user_dn, {"userAccountControl": Aduser.ADS_UF_NORMAL_ACCOUNT})
 
     def update_password_ad_user(self, user_dn, newpassword):
         unicode_pass = '"%s"' % newpassword
